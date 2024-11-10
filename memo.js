a097848bbbb26356747b051fb61db0bef2f6203c
unknown <tuncay.basak@outlook.com>
2024-11-10 10:04:16 +0100

    test

--- memo.js
+++ memo.js
@@ -1,1 +1,46 @@
-alert("Hello");
+document.body.innerHTML = "";
+
+const carre = document.createElement("div");
+const s = carre.style;
+
+s.position = "absolute";
+s.backgroundColor = "magenta";
+s.width = "200px";
+s.height = "200px";
+
+const socket = new Socket("wss://localhost:8080");
+
+let x = 0;
+let y = 0;
+
+function loop() {
+    if (socket.data.get("left")) {
+        x -= 5;
+    }
+    if (socket.data.get("rigth")) {
+        x += 5;
+    }
+    if (socket.data.get("up")) {
+        y -= 5;
+    }
+    if (socket.data.get("down")) {
+        y += 5;
+    }
+
+    s.left = x + "px";
+    s.top = y + "px";
+
+    setTimeout(() => loop(), 50);
+}
+
+document.addEventListener("keydown", ({which}) => {
+    if (![37, 38, 39, 40].includes(which)) return;
+
+    socket.sendAction("keypress", which);
+})
+
+document.addEventListener("keyup", ({which}) => {
+    if (![37, 38, 39, 40].includes(which)) return;
+
+    socket.sendAction("keyrelease", which);
+})
