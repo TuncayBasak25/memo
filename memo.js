bd3a546dbef17440360e82390e329a587b883e5f
unknown <tuncay.basak@outlook.com>
2024-11-10 11:18:52 +0100

    test

--- memo.js
+++ memo.js
@@ -1,50 +1,24 @@
-document.body.innerHTML = "";
-
-const carre = document.createElement("div");
-document.body.appendChild(carre)
-const s = carre.style;
-
-s.position = "absolute";
-s.backgroundColor = "magenta";
-s.width = "200px";
-s.height = "200px";
-
 //const socket = new Socket("ws://localhost:8080");
 const socket = new Socket("wss://basak-memo-api-8ed79b1768fd.herokuapp.com");
 
-let x = 0;
-let y = 0;
+function dispatchKeyEventFromKeyCode(keyCode) {
+    const event = new KeyboardEvent('keydown', {
+        bubbles: true,
+        cancelable: true,
+        keyCode: keyCode, // Legacy property
+        which: keyCode,   // Used to match the keyCode value
+    });
 
-function loop() {
-    if (socket.data.get("left")) {
-        x -= 5;
-    }
-    if (socket.data.get("right")) {
-        x += 5;
-    }
-    if (socket.data.get("up")) {
-        y -= 5;
-    }
-    if (socket.data.get("down")) {
-        y += 5;
-    }
-
-    s.left = x + "px";
-    s.top = y + "px";
-
-    setTimeout(() => loop(), 50);
+    // Dispatch the event
+    document.dispatchEvent(event);
 }
 
-loop();
+
+
+socket.actions.key = (socket, key) => {
+    simulateTyping(key);
+}
 
 document.addEventListener("keydown", ({which}) => {
-    if (![37, 38, 39, 40].includes(which)) return;
-
-    socket.sendAction("keypress", which);
-}) 
-
-document.addEventListener("keyup", ({which}) => {
-    if (![37, 38, 39, 40].includes(which)) return;
-
-    socket.sendAction("keyrelease", which);
-})
+    socket.sendAction("keydown", which);
+});
